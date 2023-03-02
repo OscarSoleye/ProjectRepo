78774664c0acc6d96226db3003b690a22c00c46b
OscarSoleye <clouddavinci@gmail.com>
2023-03-02 12:03:57 +0000

    comitting and pushing for access to render

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
-app = Flask(__name__)
+main = Flask(__name__)
 
-@app.route("/")
+JOBS = [
+    {
+    'id' : 1,
+    'title' : 'Data Analyst',
+    'location' : 'Abuja',
+    'salary' : 'N1500'
+    },
+    {
+    'id' : 2,
+    'title' : 'Data Scientist',
+    'location' : 'Lagos',
+    'salary' : 'N2500'
+    },
+    {
+    'id' : 3,
+    'title' : 'Game Analyst',
+    'location' : 'PH',
+    'salary' : 'N2100'
+    },
+{
+    'id' : 4,
+    'title' : 'Security Analyst',
+    'location' : 'MNH',
+    }
+]
+@main.route("/")
 def index():
-    return "index page"
-
-@app.route("/hello")
-def hello_world():
-    return "<p>Everytime i Go out, you know i bring them hoes out!</p>"
-    #This will be outputted in the broweser at http://127.0.0.1:5000
+    return render_template("index.html", jobs=JOBS, coursename="Wale")
 
